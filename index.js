c6c5565fdec55aa5597fec6a32ee7be693170494
lcdft <fuzzy8ball1@gmail.com>
2025-03-10 23:08:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
 // Middleware to check the "meta" value
 app.use((req, res, next) => {
     const meta = req.headers['meta'];
-    if (meta !== 'GloryPs') {
+    if (meta !== 'GloryPso') {
         return res.status(403).send('Access denied: Invalid meta value');
     }
     next();
