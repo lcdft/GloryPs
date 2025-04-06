965df4eb2cf029c783af32cb32b5275209632e2b
lcdft <fuzzy8ball1@gmail.com>
2025-04-06 23:41:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,10 @@ app.all('/player/login/dashboard', function (req, res) {
     try {
         const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
         for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
-        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
+        if (uName[1] && uPass[1]) {
+            tData['session_time'] = Date.now();
+            res.redirect('/player/growid/login/validate');
+        }
     } catch (why) { console.log(`Warning: ${why}`); }
 
     res.render(__dirname + '/public/html/dashboard.ejs', { data: tData });
@@ -45,8 +48,9 @@ app.all('/player/growid/login/validate', (req, res) => {
     const growId = req.body.growId;
     const password = req.body.password;
 
+    const uniqueTimestamp = Date.now();
     const token = Buffer.from(
-        `_token=${_token}&growId=${growId}&password=${password}`,
+        `_token=${_token}&growId=${growId}&password=${password}&timestamp=${uniqueTimestamp}`,
     ).toString('base64');
 
     res.send(
